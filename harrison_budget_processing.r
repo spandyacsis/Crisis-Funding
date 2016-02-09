8c8b4c17b18a47ee10ba7816381bdac521548e57
Greg Sanders <gregory.sanders@gmail.com>
2016-02-08 23:28:01 -0500

    Finished long conversion iteration 1. All Fiscal Years broken out but keeping all budget types as separate columns.

--- harrison_budget_processing.r
+++ harrison_budget_processing.r
@@ -19,19 +19,21 @@ options(warn=1)
 # debug(apply_lookups)
 # debug(CreateDuration)
 #*************************************Lookup Files*****************************************************
-Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
+# Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
 # Path<-"~\\FPDS\\R scripts and data\\"
-# Path<-"C:\\Users\\Greg Sanders\\SkyDrive\\Documents\\R Scripts and Data SkyDrive\\"
+Path<-"C:\\Users\\Greg Sanders\\SkyDrive\\Documents\\R Scripts and Data SkyDrive\\"
 
 require(plyr)
 require(reshape2)
+
 source(paste(Path,"helper.r",sep=""))
 source(paste(Path,"lookups.r",sep=""))
 source(paste(Path,"helper.r",sep=""))
 source(paste(Path,"statistics_aggregators.r",sep=""))
 source(paste(Path,"create_procedural_graphs.r",sep=""))
 
-setwd("K:\\Development\\Budget")
+# setwd("K:\\Development\\Budget")
+setwd("C:\\Users\\Greg Sanders\\Documents\\Budget")
 
 # debug(create_procedural_graphs
 Procurement<-read.csv(
@@ -44,6 +46,7 @@ Procurement<-melt(Procurement
       , id=.(ID
           ,Account
           ,Budget.Activity
+          ,Budget.Activity.Title
           ,BSA
           ,BSA.Title
           ,Line.Item
@@ -54,26 +57,56 @@ Procurement<-melt(Procurement
           ,Classified)
       )
 
-Procurement<-read.csv(
-    paste("Data\\","Harrison_Procurement_Budget_Database_Export_2015.csv",sep=""),
+
+Procurement$FiscalYear<-as.numeric(substring(as.character(Procurement$variable),4,7))
+Procurement$variable<-substring(as.character(Procurement$variable),9,999)
+unique(Procurement$variable)
+Procurement$Type[substring(as.character(Procurement$variable),1,5)=="Quant"]<-"Quantity"
+Procurement$Type[substring(as.character(Procurement$variable),1,5)!="Quant"]<-"Dollars"
+Procurement$variable[substring(as.character(Procurement$variable),1,5)=="Quant"]<-
+    substring(as.character(Procurement$variable[substring(as.character(Procurement$variable),1,5)=="Quant"]),7,999)
+
+Procurement$variable<-as.factor(Procurement$variable)
+
+CasProcurement<-dcast(Procurement, ID
+                      +Account
+                      +Budget.Activity
+                      +Budget.Activity.Title
+                      +BSA
+                      +BSA.Title
+                      +Line.Item
+                      +Line.Item.Title
+                      +Cost.Type
+                      +Cost.Type.Title
+                      +Category
+                      +Classified
+                      +variable
+                      + FiscalYear~ Type , sum)
+
+
+
+
+
+RnD<-read.csv(
+    paste("Data\\","Harrison_RnD_Budget_Database_Export.csv",sep=""),
     header=TRUE, sep=",", na.strings="", dec=".", strip.white=TRUE, 
     stringsAsFactors=TRUE
 )
-colnames(Procurement)
-Procurement<-melt(Procurement
-                  , id=c("ID"
-                         ,"Account"
-                         ,"Budget.Activity"
-                         ,"Budget.Activity.Title" 
-                         ,"BSA"
-                         ,"BSA.Title"
-                         ,"Line.Item"
-                         ,"Line.Item.Title"
-                         ,"Cost.Type"
-                         ,"Cost.Type.Title"
-                         ,"Category"
-                         ,"Classified")
-                  ,value.name="DollarsThousands"
+colnames(RnD)
+RnD<-melt(RnD
+                  , id=.(ID
+                         ,PE
+                         ,PE.Title
+                         ,Budget.Activity
+                         ,Budget.Activity.Title
+                         ,Account
+                         ,Classified)
+                  # ,value.name="DollarsThousands"
 )
 
-Procurement<-subset(Procurement, !is.na(DollarsThousands))
+
+
+RnD$FiscalYear<-as.numeric(substring(as.character(RnD$variable),4,7))
+RnD$variable<-substring(as.character(RnD$variable),9,999)
+RnD$variable<-as.factor(RnD$variable)
+
